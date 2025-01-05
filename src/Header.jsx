06857330d09f84d88d91e8fed4f0150407bdb35e
the invictus codes <hannanhacks@gmail.com>
2025-01-05 22:57:38 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -35,7 +35,7 @@ const Header = ({
                 onClick={scrollToFirstPage}
                 className="hidden px-8 hover:cursor-pointer w-[200px]  border border-red-700 z-30 py-4 bg-neutral-700 rounded-md text-white relative font-semibold after:-z-20 after:absolute after:h-1 after:w-1 after:bg-red-700 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#be123c;] hover:[text-shadow:2px_2px_2px_#fda4af] text-2xl"
               >
-                Hannan Siddiqui
+                The_Invictus
               </div>
             </div> 
 
@@ -50,7 +50,7 @@ const Header = ({
               
             >
               <div className="text-red-700 flex justify-evenly items-start ">
-              <div className=" italic">Hannan</div>
+              <div className=" italic">The_Invictus</div>
               <div onClick={toggleItems} className="ml-32">{showItems ? <RxCross2 /> : <CiMenuBurger />}</div>
               </div>
                
